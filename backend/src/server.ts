3d55c4573064e557be2311828ec4738d2814a113
shashishsoni <shshshsoni2003@tutanota.com>
2025-03-03 02:34:32 +0530

    backend back and front connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:3000', // Allow requests from your frontend URL
+    origin: ['http://localhost:3000', 'https://project-qr-code.onrender.com', 'https://project-qr-code-pearl.vercel.app'], // Allow requests from your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
     credentials: true // Allow credentials if needed
 }));
